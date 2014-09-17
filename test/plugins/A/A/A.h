c255d342de81b56b329d9816dc0fc3dd325a7069
Greg Thornton <xdissent@me.com>
2014-09-17 07:45:25 -0500

    Initial Commit

---
+++ test/plugins/A/A/A.h
@@ -0,0 +1,13 @@
+//
+//  A.h
+//  A
+//
+//  Created by Greg Thornton on 9/17/14.
+//  Copyright (c) 2014 cocoapods-cordova. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface A : NSObject
+
+@end
