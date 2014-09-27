316cd8d1477cb52161100c837eae2963a5f2c4cb
Greg Thornton <xdissent@me.com>
2014-09-27 13:59:57 -0500

    Remove special mangling due to upstream bug fixed in CocoaPods/cocoapods-packager#38

--- lib/cocoapods-cordova/builder.rb
+++ lib/cocoapods-cordova/builder.rb
@@ -60,33 +60,6 @@ module Pod
         raise RuntimeError, "Command exited abnormally: #{cmd}"
       end
 
-      # Overridden because the lib name matcher is borked upstream
-      # TODO: Send PR
-      def _mangle_for_pod_dependencies(pod_name, sandbox_root)
-        pod_libs = Dir.glob("#{sandbox_root}/build/libPods-*.a").reject do
-          |file| file == "#{sandbox_root}/build/libPods-#{@spec.name}.a"
-        end
-
-        dummy_alias = Symbols.alias_symbol "PodsDummy_Pods_#{pod_name}", pod_name
-        all_syms = [dummy_alias]
-
-        pod_libs.each do |pod_lib|
-          syms = Symbols.symbols_from_library(pod_lib)
-          all_syms += syms.map! { |sym| Symbols.alias_symbol sym, pod_name }
-        end
-
-        "GCC_PREPROCESSOR_DEFINITIONS='${inherited} #{all_syms.uniq.join(' ')}'"
-      end
-
-      # Overridden to use custom mangler
-      def build_with_mangling
-        UI.puts 'Mangling symbols'
-        defines = _mangle_for_pod_dependencies(@spec.name, @sandbox_root)
-        UI.puts 'Building mangled framework'
-        xcodebuild(defines)
-        defines
-      end
-
       # Overridden to use custom exec with output
       def build_static_lib_for_ios(static_libs, defines, output)
         exec! "libtool -static -o #{@sandbox_root}/build/package.a #{static_libs.join(' ')}"
